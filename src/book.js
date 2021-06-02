a895458e2d80724a8a61baf90183beace8bd0530
Eduardo <jeduardorjx@gmail.com>
2021-06-01 20:00:20 -0600

    Create functions based of tests for books

--- src/book.js
+++ src/book.js
@@ -1,12 +1,50 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name,
+    age,
+    pronouns
+  }
+}
+
+function saveReview(review, reviews) {
+  if (reviews.includes(review)) {
+    return reviews
+  }
+  return reviews.push(review)
+}
+
+function calculatePageCount(title) {
+  const titleLength = title.length
+  return titleLength * 20
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  const pageCount = calculatePageCount(bookTitle)
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount,
+    genre
+  }
+}
+
+function editBook(book) {
+  const newPageCount = book.pageCount * 0.75
+  book.pageCount = newPageCount
+  return {
+    ...book
+  }
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
